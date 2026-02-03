7f3736662c6f46e33935639196c03aa88500e276
JonathanGzzBen <jonathangzzben@gmail.com>
2026-02-03 14:14:56 -0600

    Add sprinting

--- player.gd
+++ player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const CAMERA_SENSITIVITY = 0.0025
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 
+var speed: float = WALK_SPEED
 var t_bob: float = 0.0
 
 @onready var head: Node3D = $Head
@@ -30,6 +32,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Handle speed
+	speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -37,8 +42,8 @@ func _physics_process(delta: float) -> void:
 	
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
